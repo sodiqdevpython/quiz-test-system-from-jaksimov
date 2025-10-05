1ab18c420308e0be479861bee59c08b1028ffe46
Sodiq Oydinov <sodiqdevpython@gmail.com>
2025-10-05 12:12:59 +0500

    sw.js to'g'irlandi backenddan

--- static/sw.js
+++ static/sw.js
@@ -2,12 +2,13 @@ importScripts("https://www.gstatic.com/firebasejs/11.0.1/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/11.0.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyAd2vyJI_dNy1D0mmPTkRB4u1Tqz-KVylc",
-  authDomain: "notify-89a18.firebaseapp.com",
-  projectId: "notify-89a18",
-  storageBucket: "notify-89a18.firebasestorage.com",
-  messagingSenderId: "197374439129",
-  appId: "1:197374439129:web:ea78742dbb9bc3af2218c8"
+  apiKey: "AIzaSyDWFQDDs6xT5phXlKWYoB1YF9WEx8fMRTU",
+  authDomain: "test-e56b8.firebaseapp.com",
+  projectId: "test-e56b8",
+  storageBucket: "test-e56b8.firebasestorage.app",
+  messagingSenderId: "572446532812",
+  appId: "1:572446532812:web:f9e2e584bc1ec95cf48e69",
+  measurementId: "G-66RZYQJ9F8"
 });
 
 const messaging = firebase.messaging();
