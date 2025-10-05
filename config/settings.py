051cda6249e45442f88e506a175dc1c86bee9b8a
Sodiq Oydinov <sodiqdevpython@gmail.com>
2025-10-05 18:21:19 +0500

    media to'g'irlandi

--- config/settings.py
+++ config/settings.py
@@ -170,8 +170,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
-
-MEDIA_FILES_DIRS = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
